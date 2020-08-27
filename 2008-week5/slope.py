a9cb4980148cff23b55bbbe67d7a19967bca2dc1
jaySHKorea <seung2586@gmail.com>
2020-08-27 16:07:40 +0900

    Complete slope.py

--- 2008-week5/slope.py
+++ 2008-week5/slope.py
@@ -1,4 +1,5 @@
 # 백준 14890번 문제
+
 '''
 크기가 N×N인 지도가 있다. 지도의 각 칸에는 그 곳의 높이가 적혀져 있다. 
 
@@ -27,3 +28,79 @@
 
 #출력
 #첫째 줄에 지나갈 수 있는 길의 개수를 출력한다.
+
+'''
+3  3  3  3  3  3
+2  3  3  3  3  3
+2  2  2  3  2  3
+1  1  1  2  2  2
+1  1  1  3  3  1
+1  1  2  3  3  2
+'''
+
+import sys
+
+def checkEqual1(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == rest for rest in iterator)
+
+def line_search(N,L,data):
+    n_slope = [0]*100
+    j = 1
+    prev = data[0]
+    while (True):
+        if ( j == N ): return 1
+        now = data[j]
+        # 경사가 달라졌을 때
+        if ( abs(prev-now) == 1 ):
+            # 경사가 높아졌고, 경사 차이가 1이고, 경사로를 뒤로 놓을만큼 공간이 있을 때
+            if ( prev < now and j-L >= 0 ):
+                if ( checkEqual1(data[j-L:j]) == True and not 1 in n_slope[j-L:j]): # 뒤로 놓을만큼의 공간의 경사로가 동일할 때
+                    n_slope[j-L:j] = [1]*L
+                    prev = now
+                    j += 1
+                else:
+                    return 0
+            # 경사가 낮아졌고, 경사 차이가 1이고, 경사로를 앞으로 놓을만큼 공간이 있을 때
+            elif ( prev > now and j+L <= N ):
+                if  ( checkEqual1(data[j:j+L]) == True and not 1 in n_slope[j:j+L]):
+                    n_slope[j:j+L] = [1]*L
+                    j = j+L
+                    prev = data[j-1]
+                else:
+                    return 0
+            else:
+                return 0
+        # 경사가 그대로일때
+        elif ( prev == now ):
+            prev = now
+            j += 1
+        else: # 해당되지 않는 경우 out
+            return 0
+
+def solution(N,L,slope_map):
+    answer = 0
+    
+    # 가로
+    for i in range(0,N): # 가로 한줄에 대한 탐색
+       answer += line_search(N,L,slope_map[i])
+    
+    # 세로
+    for i in range(0,N):
+        answer += line_search(N,L,[lst[i] for lst in slope_map])     
+    
+    return answer
+
+if __name__ == '__main__':
+    slope_map = []
+    var = list(map(int,sys.stdin.readline().rstrip().split(" ")))
+    N = var[0]# 배열크키
+    L = var[1]# 경사로 길이
+    for i in range(0,N):
+        slope_map.append(list(map(int,sys.stdin.readline().rstrip().split()))) # 경사로 배열 입력 받기 list(map(int, list_a))
+    
+    print(solution(N,L,slope_map))
