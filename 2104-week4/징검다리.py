527894b868ef32dba6de2297999710a2f13f8e16
jaySHKorea <seung2586@gmail.com>
2021-04-30 22:56:21 +0900

    징검다리 2회차

--- 2104-week4/징검다리.py
+++ 2104-week4/징검다리.py
@@ -1,5 +1,28 @@
 ''' 이진 탐색 2회차 '''
+import math
 
 def solution(distance,rocks,n):
     answer = 0
+    rocks.sort()
+    rocks.append(distance)
+    left, right = 0, distance
+
+    while left <= right:
+        prev = 0
+        mins = math.inf
+        removed = 0
+
+        mid = int((left+right)/2)
+        for r in rocks:
+            if r-prev < mid:
+                removed += 1
+            else: 
+                mins = min(mins,r-prev)
+                prev = r
+
+        if removed > n:
+            right = mid-1
+        else:
+            answer = max(answer,mins)
+            left = mid+1
     return answer
