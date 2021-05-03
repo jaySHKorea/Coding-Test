684f82fe3a32872ef2454c275464d0575a2b5427
jaySHKorea <seung2586@gmail.com>
2021-05-03 21:04:43 +0900

    주석 수정

--- 2104-week4/메뉴리뉴얼.py
+++ 2104-week4/메뉴리뉴얼.py
@@ -1,6 +1,7 @@
 import itertools
 import heapq
 
+# 구현
 def solution(orders, course):
     answer = []
     dict = {}
@@ -9,26 +10,27 @@ def solution(orders, course):
     for ord in orders:
         order = list(ord)
         order.sort()
-        for c in course:
+        for c in course: # 해당 주문에서 course 개수만큼 가능한 조합
             if len(order) < c:
                 continue
             combs = itertools.combinations(order,c)
-            for comb in combs:
+            for comb in combs: # dict에 조합 기록
                 comb = ''.join(comb)
                 if comb in dict.keys():
                     dict[comb] += 1
                 else:
                     dict[comb] = 1
     
+    # 가장 많이 주문된 조합들 찾기
     for key in dict.keys():
         cnum = len(key)
         if most[cnum] and dict[key] >= 2:
-            if most[cnum][0][0] == dict[key]:
+            if most[cnum][0][0] == dict[key]: # 같은 최대 횟수
                 heapq.heappush(most[cnum],(dict[key],key))
-            elif most[cnum][0][0] < dict[key]:
+            elif most[cnum][0][0] < dict[key]: # 큰게 나타났을 때
                 most[cnum] = []
                 heapq.heappush(most[cnum],(dict[key],key))
-        elif dict[key] >= 2:
+        elif dict[key] >= 2: # 처음
             heapq.heappush(most[cnum],(dict[key],key))
 
     for c in course:
